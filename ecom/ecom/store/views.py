ed35e488430244c6255de2f6d55e2285c6145c3d
SAADANAM <saadanam2004@gmail.com>
2024-05-15 14:52:34 +0100

    site web

---
+++ ecom/ecom/store/views.py
@@ -0,0 +1,269 @@
+from django.shortcuts import render, redirect
+from .models import Product, Category, Profile
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
+import google.generativeai as genai
+from django.contrib.sessions.models import Session
+from payment.forms import ShippingForm
+from payment.models import ShippingAddress
+
+from django import forms
+from django.db.models import Q
+import json
+from cart.cart import Cart
+
+
+def search(request):
+	# Determine if they filled out the form
+	if request.method == "POST":
+		searched = request.POST['searched']
+		# Query The Products DB Model
+		searched = Product.objects.filter(Q(name__icontains=searched) | Q(description__icontains=searched))
+		# Test for null
+		if not searched:
+			messages.success(request, "That Product Does Not Exist...Please try Again.")
+			return render(request, "search.html", {})
+		else:
+			return render(request, "search.html", {'searched':searched})
+	else:
+		return render(request, "search.html", {})	
+
+
+def update_info(request):
+	if request.user.is_authenticated:
+		# Get Current User
+		current_user = Profile.objects.get(user__id=request.user.id)
+		# Get Current User's Shipping Info
+		shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+		
+		# Get original User Form
+		form = UserInfoForm(request.POST or None, instance=current_user)
+		# Get User's Shipping Form
+		shipping_form = ShippingForm(request.POST or None, instance=shipping_user)		
+		if form.is_valid() or shipping_form.is_valid():
+			# Save original form
+			form.save()
+			# Save shipping form
+			shipping_form.save()
+
+			messages.success(request, "Your Info Has Been Updated!!")
+			return redirect('home')
+		return render(request, "update_info.html", {'form':form, 'shipping_form':shipping_form})
+	else:
+		messages.success(request, "You Must Be Logged In To Access That Page!!")
+		return redirect('home')
+
+
+
+def update_password(request):
+	if request.user.is_authenticated:
+		current_user = request.user
+		# Did they fill out the form
+		if request.method  == 'POST':
+			form = ChangePasswordForm(current_user, request.POST)
+			# Is the form valid
+			if form.is_valid():
+				form.save()
+				messages.success(request, "Your Password Has Been Updated...")
+				login(request, current_user)
+				return redirect('update_user')
+			else:
+				for error in list(form.errors.values()):
+					messages.error(request, error)
+					return redirect('update_password')
+		else:
+			form = ChangePasswordForm(current_user)
+			return render(request, "update_password.html", {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In To View That Page...")
+		return redirect('home')
+def update_user(request):
+	if request.user.is_authenticated:
+		current_user = User.objects.get(id=request.user.id)
+		user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
+		if user_form.is_valid():
+			user_form.save()
+
+			login(request, current_user)
+			messages.success(request, "User Has Been Updated!!")
+			return redirect('home')
+		return render(request, "update_user.html", {'user_form':user_form})
+	else:
+		messages.success(request, "You Must Be Logged In To Access That Page!!")
+		return redirect('home')
+
+
+def category_summary(request):
+	categories = Category.objects.all()
+	return render(request, 'category_summary.html', {"categories":categories})	
+
+def category(request, foo):
+	# Replace Hyphens with Spaces
+	foo = foo.replace('-', ' ')
+	# Grab the category from the url
+	try:
+		# Look Up The Category
+		category = Category.objects.get(name=foo)
+		products = Product.objects.filter(category=category)
+		return render(request, 'category.html', {'products':products, 'category':category})
+	except:
+		messages.success(request, ("That Category Doesn't Exist..."))
+		return redirect('home')
+
+
+def product(request,pk):
+	product = Product.objects.get(id=pk)
+	return render(request, 'product.html', {'product':product})
+
+
+def home(request):
+	products = Product.objects.all()
+	return render(request, 'home.html', {'products':products})
+
+
+def about(request):
+	return render(request, 'about.html', {})	
+
+def login_user(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+
+			# Do some shopping cart stuff
+			current_user = Profile.objects.get(user__id=request.user.id)
+			# Get their saved cart from database
+			saved_cart = current_user.old_cart
+			# Convert database string to python dictionary
+			if saved_cart:
+				# Convert to dictionary using JSON
+				converted_cart = json.loads(saved_cart)
+				# Add the loaded cart dictionary to our session
+				# Get the cart
+				cart = Cart(request)
+				# Loop thru the cart and add the items from the database
+				for key,value in converted_cart.items():
+					cart.db_add(product=key, quantity=value)
+
+			messages.success(request, ("You Have Been Logged In!"))
+			return redirect('home')
+		else:
+			messages.success(request, ("There was an error, please try again..."))
+			return redirect('login')
+
+	else:
+		return render(request, 'login.html', {})
+
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, ("You have been logged out...Thanks for stopping by..."))
+	return redirect('home')
+
+
+
+def register_user(request):
+	form = SignUpForm()
+	if request.method == "POST":
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			# log in user
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, ("Username Created - Please Fill Out Your User Info Below..."))
+			return redirect('update_info')
+		else:
+			messages.success(request, ("Whoops! There was a problem Registering, please try again..."))
+			return redirect('register')
+	else:
+		return render(request, 'register.html', {'form':form})
+	
+
+
+#def chatbot(request):
+		#return render(request, 'chatbot.html', {})	
+
+
+def chatbot(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    if request.method == 'GET':
+        # Clear chat history if it exists in the session
+        if 'chat' in request.session:
+            del request.session['chat']
+        # Define an empty chat list
+        chat = []
+    elif request.method == 'POST':
+        user_message = request.POST.get('user_message')
+        bot_response = interact_with_chatbot(user_message)
+        
+        # Initialize chat history if it doesn't exist in the session
+        chat = request.session.get('chat', [])
+        
+        # Append user message and bot response to chat history
+        chat.append({"sender": "user", "text": user_message})
+        chat.append({"sender": "bot", "text": bot_response})
+
+        # Save updated chat history to session
+        request.session['chat'] = chat
+
+    return render(request, 'chatbot.html', {'chat': chat})
+
+def interact_with_chatbot(user_message):
+    genai.configure(api_key="AIzaSyAYpJdnooqa1EZakSy_mdCa8tCAOoiMBxo")
+
+    # Set up the model
+    generation_config = {
+        "temperature": 0.9,
+        "top_p": 1,
+        "top_k": 1,
+        "max_output_tokens": 2048,
+    }
+
+    safety_settings = [
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+    ]
+
+    system_instruction = "you are a friendly assistant to a website called \"Bi3smart\", you are an expert in fashion, if the user asks for t-shirts, tell him that our site has four t-shirts : one is white and it costs 14 dollar, one is black and it costs 17 dollar, one is dark green and it costs 23.99 dollar, and the last is a black one with symbols on it and it costs 10 dollar.\nif the user asks for sweaters, tell him we have three sweaters : one is white and it costs 39 dollar, one is pink and it costs 50 dollar, one is black and it costs 35.99 dollar.\nif the user asks for shoes, we dont have shoes in our website and we will have them soon and if he asks for any other clothing item, tell him, we dont have it currently but we'll make sure to get it\nand everytime you reply to the user, tell the user \"i wish u had a pleasurable experience\" without asking for any other information"
+
+
+
+
+    model = genai.GenerativeModel(
+        model_name="gemini-1.5-pro-latest",
+        generation_config=generation_config,
+        system_instruction=system_instruction,
+        safety_settings=safety_settings
+    )
+
+    convo = model.start_chat(history=[])
+
+    convo.send_message(user_message)
+
+    # Retrieve response from the chat
+    response = convo.last.text
+    return response
